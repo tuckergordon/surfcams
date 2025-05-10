cb5f7661c0f7170d5a27e2d28a63562dc0067021
tuckergordon <tucker.gordon18@gmail.com>
2025-05-10 12:04:21 -0400

    allowfullscreen

--- src/App.svelte
+++ src/App.svelte
@@ -21,6 +21,7 @@
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
         ></iframe>
       {:else}
         <div
